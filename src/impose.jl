572efc0b9bb1adc70fe3742a6df55c02b4d652a9
wangjie212 <55985924+wangjie212@users.noreply.github.com>
2021-04-04 20:56:24 +0800

    Add files via upload

--- src/impose.jl
+++ src/impose.jl
@@ -1,33 +1,21 @@
 function posepsd!(model, mvar, tsupp, L)
-    nb = 8
+    nb = 6
     ltsupp = length(tsupp)
     Pauli = Vector{Matrix{ComplexF16}}(undef, 4)
     Pauli[1] = [1 0; 0 1]
     Pauli[2] = [0 1; 1 0]
     Pauli[3] = [0 -im; im 0]
     Pauli[4] = [1 0; 0 -1]
-    rfbcons = zeros(GenericAffExpr{Float64,VariableRef}, 2^nb, 2^nb)
-    ifbcons = zeros(GenericAffExpr{Float64,VariableRef}, 2^nb, 2^nb)
-    for i = 0:3, j = 0:3, k = 0:3, l = 0:3, s = 0:3, t = 0:3, u = 0:3, v = 0:3
-        mon = mono([i,j,k,l,s,t,u,v])
+    pos = zeros(GenericAffExpr{Float64,VariableRef}, 2^nb, 2^nb)
+    for i = 0:3, j = 0:3, k = 0:3, l = 0:3, s = 0:3, t = 0:3
+        mon = mono([i,j,k,l,s,t])
         if !iszero(mon)
             Locb = bfind(tsupp, ltsupp, reduce4(mon, L))
-            tp = tensor([Pauli[i+1], Pauli[j+1], Pauli[k+1], Pauli[l+1], Pauli[s+1], Pauli[t+1], Pauli[u+1], Pauli[v+1]])
-            for p = 1:2^nb, q = p:2^nb
-                rtpcoe,itpcoe = reim(tp[p,q])
-                if rtpcoe != 0
-                    rfbcons[p,q] += rtpcoe*mvar[Locb]
-                elseif itpcoe != 0
-                    ifbcons[p,q] += itpcoe*mvar[Locb]
-                end
-            end
+            tp = tensor([Pauli[i+1], Pauli[j+1], Pauli[k+1], Pauli[l+1], Pauli[s+1], Pauli[t+1]])
+            pos += mvar[Locb]*tp
         end
     end
-    pos = [AffExpr(0) for i=1:2*2^nb, j=1:2*2^nb]
-    pos[1:2^nb,1:2^nb] = rfbcons
-    pos[2^nb+1:2*2^nb,2^nb+1:2*2^nb] = rfbcons
-    pos[1:2^nb,2^nb+1:2*2^nb] = ifbcons' - ifbcons
-    @constraint(model, Symmetric(pos) in PSDCone())
+    @constraint(model, pos in PSDCone())
 end
 
 function tensor(matrices)
