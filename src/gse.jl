90885ee2bfd00765166df08f3340540f7249168e
wangjie212 <55985924+wangjie212@users.noreply.github.com>
2021-04-14 01:49:47 +0800

    Add files via upload

--- src/gse.jl
+++ src/gse.jl
@@ -12,22 +12,22 @@ function GSE1(supp::Vector{Vector{UInt16}}, coe::Vector{Float64}, L::Int, d::Int
             end
         end
     end
-    if posepsd == true
-        # for i = 0:3, j = 0:3, k = 0:3, l = 0:3, s = 0:3, t = 0:3
-        #     sites = [[1;3;4;5;6;7], [1;2;4;5;6;7], [1;2;3;5;6;7]]
-        #     for r = 1:3
-        #         mon = mono([i,j,k,l,s,t], sites=sites[r])
-        #         if !iszero(mon)
-        #             push!(tsupp, reduce4(mon, L))
-        #         end
-        #     end
-        # end
-        for i = 0:3, j = 0:3, k = 0:3, l = 0:3, s = 0:3, t = 0:3, u = 0:3, v = 0:3
-            mon = mono([i,j,k,l,s,t,u,v])
-            if !iszero(mon)
-                push!(tsupp, reduce4(mon, L))
+    if posepsd == true && lattice == "chain"
+        for i = 0:3, j = 0:3, k = 0:3, l = 0:3, s = 0:3, t = 0:3
+            sites = [[1;3;4;5;6;7], [1;2;4;5;6;7], [1;2;3;5;6;7]]
+            for r = 1:3
+                mon = mono([i,j,k,l,s,t], sites=sites[r])
+                if !iszero(mon)
+                    push!(tsupp, reduce4(mon, L))
+                end
             end
         end
+        # for i = 0:3, j = 0:3, k = 0:3, l = 0:3, s = 0:3, t = 0:3, u = 0:3, v = 0:3
+        #     mon = mono([i,j,k,l,s,t,u,v])
+        #     if !iszero(mon)
+        #         push!(tsupp, reduce4(mon, L))
+        #     end
+        # end
     end
     sort!(tsupp)
     unique!(tsupp)
@@ -195,11 +195,15 @@ function GSE1(supp::Vector{Vector{UInt16}}, coe::Vector{Float64}, L::Int, d::Int
     end
     @constraint(model, mvar[1]==1)
     if posepsd == true
-        # posepsd6!(model, mvar, tsupp, L)
-        # posepsd6!(model, mvar, tsupp, L, sites=[1;3;4;5;6;7])
-        # posepsd6!(model, mvar, tsupp, L, sites=[1;2;4;5;6;7])
-        # posepsd6!(model, mvar, tsupp, L, sites=[1;2;3;5;6;7])
-        posepsd8!(model, mvar, tsupp, L)
+        if lattice == "chain"
+            posepsd6!(model, mvar, tsupp, L)
+            posepsd6!(model, mvar, tsupp, L, sites=[1;3;4;5;6;7])
+            posepsd6!(model, mvar, tsupp, L, sites=[1;2;4;5;6;7])
+            posepsd6!(model, mvar, tsupp, L, sites=[1;2;3;5;6;7])
+            # posepsd8!(model, mvar, tsupp, L)
+        else
+            posepsd6!(model, mvar, tsupp, L, lattice="square")
+        end
     end
     if energy != []
         gsen = AffExpr(0)
--- src/impose.jl
+++ src/impose.jl
@@ -1,11 +1,15 @@
-function posepsd6!(model, mvar, tsupp, L; sites=[1;2;3;4;5;6])
+function posepsd6!(model, mvar, tsupp, L; lattice="chain", sites=[1;2;3;4;5;6])
     ltsupp = length(tsupp)
     Pauli = [[1 0; 0 1], [0 1; 1 0], [0 -im; im 0], [1 0; 0 -1]]
     pos = zeros(GenericAffExpr{Float64,VariableRef}, 2^6, 2^6)
     for i = 0:3, j = 0:3, k = 0:3, l = 0:3, s = 0:3, t = 0:3
         mon = mono([i,j,k,l,s,t], sites=sites)
         if !iszero(mon)
-            Locb = bfind(tsupp, ltsupp, reduce4(mon, L))
+            if lattice=="chain"
+                Locb = bfind(tsupp, ltsupp, reduce4(mon, L))
+            else
+                Locb = bfind(tsupp, ltsupp, reduce5(reduce4(mon, L, lattice="square"), L))
+            end
             tp = tensor([Pauli[i+1], Pauli[j+1], Pauli[k+1], Pauli[l+1], Pauli[s+1], Pauli[t+1]])
             pos += mvar[Locb]*tp
         end
